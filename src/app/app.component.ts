8afa4b0e69bba87fb0623ee8bbd3de81e54f9dcd
Logan Murray <lpmurray16@users.noreply.github.com>
2023-09-23 14:38:48 -0500

    sorting by store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Item, RealtimeDatabaseService,  } from './services/realtime-database.service';
+import {
+  Item,
+  RealtimeDatabaseService,
+} from './services/realtime-database.service';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   items: Observable<Item[]>;
   sortedItems: Item[] = [];
+  originalSortedItems: Item[] = [];
   completedItems: Item[] = [];
+  filterByStoreSelection: string = 'all';
 
   itemToAdd: Item = {
     itemName: '',
@@ -20,7 +25,7 @@ export class AppComponent implements OnInit {
   };
 
   @ViewChild('firstInput') firstInput!: ElementRef;
-  
+
   constructor(private realtimeDb: RealtimeDatabaseService) {
     this.items = this.realtimeDb.getItems();
   }
@@ -29,6 +34,7 @@ export class AppComponent implements OnInit {
     this.items.subscribe((items) => {
       this.sortedItems = items.sort((a, b) => a.sortOrder - b.sortOrder);
       this.sortedItems = this.sortedItems.filter((item) => !item.purchased);
+      this.originalSortedItems = this.sortedItems;
       this.completedItems = items.filter((item) => item.purchased);
     });
   }
@@ -36,9 +42,13 @@ export class AppComponent implements OnInit {
   addItem() {
     this.itemToAdd.sortOrder = this.sortedItems.length;
 
-    this.itemToAdd.itemName = this.itemToAdd.itemName.charAt(0).toUpperCase() + this.itemToAdd.itemName.slice(1);
-    this.itemToAdd.storeName = this.itemToAdd.storeName.charAt(0).toUpperCase() + this.itemToAdd.storeName.slice(1);
-    
+    this.itemToAdd.itemName =
+      this.itemToAdd.itemName.charAt(0).toUpperCase() +
+      this.itemToAdd.itemName.slice(1);
+    this.itemToAdd.storeName =
+      this.itemToAdd.storeName.charAt(0).toUpperCase() +
+      this.itemToAdd.storeName.slice(1);
+
     this.realtimeDb.addItemToDb(this.itemToAdd);
     this.clearItemToAdd();
     this.firstInput.nativeElement.focus();
@@ -67,4 +77,23 @@ export class AppComponent implements OnInit {
     });
   }
 
+  onFilterChange() {
+    let filterSelection = this.filterByStoreSelection.toLowerCase();
+    if (filterSelection === 'all') {
+      this.sortedItems = this.originalSortedItems;
+      return;
+    }
+    this.sortedItems = this.originalSortedItems.sort((a, b) => {
+      if (
+        filterSelection &&
+        a.storeName.toLowerCase().includes(filterSelection)
+      ) {
+        return -1;
+      } else if (b.storeName.toLowerCase().includes(filterSelection)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 }
